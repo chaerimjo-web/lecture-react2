46e940625e85cc80f29522c07e6a2e0288130aa1
chaerimjo-web <chaerim010710@gmail.com>
2025-06-10 10:49:05 +0900

    환경변수 설정

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const App = () => (
+  <div className="productPage">
+    <div className="Page">
+      <header>
+        <h1>메뉴목록</h1>
+      </header>
+      <main>
+        <ul>
+          <li>
+            <div className="ProductItem">
+              <div className="description">
+                <h2>해물 계란 라면</h2>
+                <div>가격: 6,000원</div>
+                <button className="Button brand">주문하기</button>
+              </div>
+              <div className="thumbnail">
+                <img
+                  src="./images/menu-고소한바질파스타.jpg"
+                  alt="고소한바질파스타 6,000원"
+                />
+              </div>
+            </div>
+          </li>
+        </ul>
+      </main>
+      <footer>
+        <nav className="Navbar">
+          <a className="active" href="#">
+            메뉴목록
+          </a>
+          <a href="#">주문내역</a>
+        </nav>
+      </footer>
+    </div>
+  </div>
+);
+
+export default App;
