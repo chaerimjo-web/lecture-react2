1f9004b2d1bb2ad44a48b42fb664ede37bf830d3
chaerimjo-web <chaerim010710@gmail.com>
2025-06-11 11:32:10 +0900

    App 내부 옮기기

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,6 @@
 import React from "react";
 import ProductPage from "./pages/ProductPage";
 
-<<<<<<< HEAD
 const App = () => <ProductPage />;
-=======
-const fakeProduct = {
-  id: "CACDA421",
-  name: "해물 계란 라면",
-  price: 6000,
-  thumbnail: "./images/menu-해물계란라면.jpg",
-};
-
-const App = () => (
-  <div className="ProductPage">
-    <div className="Page">
-      <header>
-        <h1>메뉴목록</h1>
-      </header>
-      <main>
-        <ul>
-          <li>
-            <ProductItem product={fakeProduct} />
-          </li>
-        </ul>
-      </main>
-      <footer>
-        <nav className="Navbar">
-          <a className="active" href="#">
-            메뉴목록
-          </a>
-          <a href="#">주문내역</a>
-        </nav>
-      </footer>
-    </div>
-  </div>
-);
->>>>>>> 94c0df4446706b3f48b78cfcb4d8879a33dabb5e
 
 export default App;
