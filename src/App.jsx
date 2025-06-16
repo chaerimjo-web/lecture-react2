6d90521799de2fd1b67fd9bc120bfb52d4e0ad43
chaerimjo-web <chaerim010710@gmail.com>
2025-06-16 14:44:39 +0900

    CartPage 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import OrderPage from "./pages/OrderPage";
 import ProductPage from "./pages/ProductPage";
+import CartPage from "./pages/CartPage";
 
-const App = () => <OrderPage />;
-
+const App = () => {
+ return (
+ 	// <OrderPage />
+	<CartPage />
+ )
+}
 export default App;
---
+++ src/pages/CartPage/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Page from "../../components/Page";
+import Title from "../../components/Title";
+import Button from "../../components/Button";
+import ProductItem from "../../components/ProductItem";
+
+const fakeProduct = {
+  id: "CACDA421",
+  name: "해물 계란 라면",
+  price: 6000,
+  thumbnail: "./images/menu-해물계란라면.jpg",
+};
+
+const CartPage = ({ product }) => {
+  return (
+    <div className="CartPage">
+      <Page
+        header={<Title>장바구니</Title>}
+        footer={
+          <Button styleType={" brand-solid"} block>
+            결제하기
+          </Button>
+        }
+      >
+        <ProductItem product={fakeProduct}></ProductItem>
+      </Page>
+    </div>
+  );
+};
+
+export default CartPage;
