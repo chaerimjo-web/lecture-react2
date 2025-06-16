f6d1dfed72acc862c5f1545a45cd8b97ba339118
chaerimjo-web <chaerim010710@gmail.com>
2025-06-16 11:29:18 +0900

    OrderDeliveryCard 페이지 생성

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-const Card = ({header, data = [], footer}) => {
+const Card = ({ header, data = [], footer }) => {
   return (
     <div className="Card">
-      <header>{header}</header>
+      {header && <header>{header}</header>}
       <main>
-				{data.map(({term, description}) => (
-					<dl key={term}>
-						<dt>{term}</dt>
-						<dd>{description}</dd>
-					</dl>
-				))}
-			</main>
-      <footer>{footer}</footer>
+        {data.map(({ term, description }) => (
+          <dl key={term}>
+            <dt>{term}</dt>
+            <dd>{description}</dd>
+          </dl>
+        ))}
+      </main>
+      {footer && <footer>{footer}</footer>}
     </div>
   );
 };
--- src/pages/OrderPage/index.jsx
+++ src/pages/OrderPage/index.jsx
@@ -3,12 +3,30 @@ import Page from "../../components/Page";
 import Title from "../../components/Title";
 import NaveBar from "../../components/Navbar";
 import Card from "../../components/Card";
+import OrderDeliveryCard from "./OrderDeliveryCard";
+
+const fakeOrder = {
+  id: "CACDA420",
+  orderDate: "2025. 6. 16. 오전 11:04:30",
+  status: "음식 준비중",
+  name: "짜장면",
+  totalPrice: 7000,
+  paymentMethod: "마이페이",
+  productPrice: 6000,
+  deliveryPrice: 3000,
+  discountPrice: 2000,
+  deliveryAddress: "서울특별시 송파구 잠실동 1번지",
+  deliveryContact: "010-1111-2222",
+  messageToShop: "포크는 주지 마세요",
+  messageToRider: "안전하게 오세요",
+  position: [30, 30],
+};
 
 const OrderPage = () => {
   return (
     <div className="OrderPage">
       <Page header={<Title>주문내역</Title>} footer={<NaveBar />}>
-        <Card />
+        <OrderDeliveryCard order={fakeOrder} />
       </Page>
     </div>
   );
