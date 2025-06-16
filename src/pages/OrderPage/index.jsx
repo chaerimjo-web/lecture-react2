61ad2cc01f048e085aaaf70cd6b92123b01e3ba4
chaerimjo-web <chaerim010710@gmail.com>
2025-06-16 13:51:23 +0900

    상태정의하기

--- src/pages/OrderPage/index.jsx
+++ src/pages/OrderPage/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import OrderApi from "shared/api/OrderApi";
 import Page from "../../components/Page";
 import Title from "../../components/Title";
 import NaveBar from "../../components/Navbar";
@@ -7,32 +8,44 @@ import OrderDeliveryCard from "./OrderDeliveryCard";
 import OrderPaymentCard from "./OrderPaymentCard";
 import OrderStatusCard from "./OrderStatusCard";
 
-const fakeOrder = {
-  id: "CACDA420",
-  orderDate: "2025. 6. 16. 오전 11:04:30",
-  status: "음식 준비중",
-  name: "짜장면",
-  totalPrice: 7000,
-  paymentMethod: "마이페이",
-  productPrice: 6000,
-  deliveryPrice: 3000,
-  discountPrice: 2000,
-  deliveryAddress: "서울특별시 송파구 잠실동 1번지",
-  deliveryContact: "010-1111-2222",
-  messageToShop: "포크는 주지 마세요",
-  messageToRider: "안전하게 오세요",
-  position: [30, 30],
-};
+class OrderPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-const OrderPage = () => {
-  return (
-    <div className="OrderPage">
-      <Page header={<Title>주문내역</Title>} footer={<NaveBar />}>
-				<OrderStatusCard order={fakeOrder} />
-				<OrderPaymentCard order={fakeOrder}  />
-        <OrderDeliveryCard order={fakeOrder} />
-      </Page>
-    </div>
-  );
-};
+    this.state = {
+      order: null,
+    };
+  }
+
+  async fetch() {
+    try {
+      const order = await OrderApi.fetchMyOrder();
+      this.setState({ order });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  render() {
+    const { order } = this.state;
+
+    return (
+      <div className="OrderPage">
+        <Page header={<Title>주문내역</Title>} footer={<NaveBar />}>
+          {order && (
+            <>
+              <OrderStatusCard order={order} />
+              <OrderPaymentCard order={order} />
+              <OrderDeliveryCard order={order} />
+            </>
+          )}
+        </Page>
+      </div>
+    );
+  }
+}
 export default OrderPage;
