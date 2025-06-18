6698a2fa41969ab93469cb549e8e8926bc594934
chaerimjo-web <chaerim010710@gmail.com>
2025-06-18 16:34:31 +0900

    Ref 객체 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createRef } from "react";
 import OrderPage from "./pages/OrderPage";
 import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
@@ -11,4 +11,22 @@ const App = () => {
     <CartPage />
   );
 };
-export default App;
+// export default App;
+
+class MyComponent extends React.Component {
+  divRef = React.createRef();
+
+  render() {
+    return <div ref={this.divRef}>DIV</div>;
+  }
+
+  componentDidMount() {
+    console.log(this.divRef);
+    const divElement = this.divRef.current;
+    divElement.style.backgroundColor = "aqua";
+    divElement.style.height = "50px";
+    divElement.style.width = "50px";
+  }
+}
+
+export default MyComponent;
