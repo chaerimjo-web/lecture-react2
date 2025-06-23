22af706d144a6102d27650a94facd2cfe88fb017
chaerimjo-web <chaerim010710@gmail.com>
2025-06-23 16:30:05 +0900

    이벤트 에미터

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import OrderPage from "./pages/OrderPage";
 import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
 import OrderableProductItem from "./pages/ProductPage/OrderableProductItem";
+import createEventEmitter from "shared/lib/EventEmitter";
 
 const App = () => {
   return (
@@ -12,3 +13,13 @@ const App = () => {
   );
 };
 export default App;
+
+const eventEmitter = createEventEmitter(0);
+const logger = (value) => console.log(value);
+
+eventEmitter.on(logger);
+console.log(eventEmitter.get());
+eventEmitter.set(1);
+eventEmitter.set(2);
+
+setTimeout(() => eventEmitter.set(10), 3000);
