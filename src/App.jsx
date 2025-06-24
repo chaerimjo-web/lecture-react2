e179d9b2fbc60eaec3a8294ef177162c4711bf34
chaerimjo-web <chaerim010710@gmail.com>
2025-06-24 14:50:08 +0900

    조건부 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -6,68 +6,14 @@ import OrderableProductItem from "./pages/ProductPage/OrderableProductItem";
 import createEventEmitter from "shared/lib/EventEmitter";
 
 const App = () => {
+  const { pathname } = window.location;
+
   return (
-    // <ProductPage />
-    // <OrderPage />
-    <CartPage />
+    <>
+      {pathname === "/cart" && <CartPage />}
+      {pathname === "/order" && <OrderPage />}
+      {!["/cart", "/order"].includes(pathname) && <ProductPage />}
+    </>
   );
 };
-// export default App;
-
-import MyReact from "./lib/MyReact";
-
-const myReact = MyReact();
-
-const countContext = myReact.createContext({
-  count: 0,
-  setCount: () => {},
-});
-
-class CountProvider extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      count: 0,
-    };
-  }
-
-  render() {
-    const value = {
-      count: this.state.count,
-      setCount: (nextValue) => this.setState({ count: nextValue }),
-    };
-    return (
-      <countContext.Provider value={value}>
-        {this.props.children}
-      </countContext.Provider>
-    );
-  }
-}
-
-const Count = () => {
-  return (
-    <countContext.Consumer>
-      {(value) => <div>{value.count}</div>}
-    </countContext.Consumer>
-  );
-};
-
-const PlusButton = () => {
-  return (
-    <countContext.Consumer>
-      {(value) => (
-        <button onClick={() => value.setCount(value.count + 1)}>
-          + 카운트 올리기
-        </button>
-      )}
-    </countContext.Consumer>
-  );
-};
-
-export default () => (
-  <CountProvider>
-    <Count />
-    <PlusButton />
-  </CountProvider>
-);
+export default App;
