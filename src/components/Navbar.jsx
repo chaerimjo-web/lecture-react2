511f9ac53b2d1e3ca9f0d66d4505c0c955bc03a5
chaerimjo-web <chaerim010710@gmail.com>
2025-06-25 17:04:21 +0900

    경로 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import { Link } from "../lib/MyRouter";
 
 const Navbar = () => (
   <nav className="Navbar">
-    <MyRouter.Link className="active" to="#">
+    <MyRouter.Link className="active" to="/">
       메뉴목록
     </MyRouter.Link>
-    <MyRouter.Link to="#">주문내역</MyRouter.Link>
+    <MyRouter.Link to="/order">주문내역</MyRouter.Link>
   </nav>
 );
 
