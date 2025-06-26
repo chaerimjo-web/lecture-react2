b0a3513c9a757006b2868275f4aa67833dbc7f24
chaerimjo-web <chaerim010710@gmail.com>
2025-06-26 14:46:46 +0900

    고차 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import * as MyRouter from "./lib/MyRouter";
 import CartPage from "./pages/CartPage";
 import OrderPage from "./pages/OrderPage";
 import ProductPage from "./pages/ProductPage";
+import { getComponentName } from "./lib/utils";
 
 const App = () => (
   <MyRouter.Router>
@@ -14,4 +15,47 @@ const App = () => (
     </MyRouter.Routes>
   </MyRouter.Router>
 );
-export default App;
+// export default App;
+
+export class Header extends React.Component {
+  render() {
+    return <header>Header</header>;
+  }
+}
+
+export class Button extends React.Component {
+  handleClick = () => {
+    this.props.log("클릭");
+  };
+  render() {
+    return <button onClick={this.handleClick}>Button</button>;
+  }
+}
+
+const withLogging = (WrappComponent) => {
+  function log(message) {
+    console.log(`[${getComponentName(WrappComponent)}] ${message}`);
+  }
+  class WithLogging extends React.Component {
+    render() {
+      const enhancedProps = {
+        log,
+      };
+      return <WrappComponent {...this.props} {...enhancedProps} />;
+    }
+    componentDidMount() {
+      log("마운트");
+    }
+  }
+  return WithLogging;
+};
+
+const EnhancedHeader = withLogging(Header);
+const EnhancedButton = withLogging(Button);
+
+export default () => (
+  <>
+    <EnhancedHeader />
+    <EnhancedButton />
+  </>
+);
--- src/pages/ProductPage/OrderableProductItem.jsx
+++ src/pages/ProductPage/OrderableProductItem.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ProductItem from "../../components/ProductItem";
+import * as MyRouter from "../../lib/MyRouter";
 
-const OrderableProductItem = ({ product }) => {
-  const handleClick = () => {
-    console.log("장바구니로 이동");
-  };
-
-  return <ProductItem product={product} onClick={handleClick} />;
-};
+const OrderableProductItem = ({ product }) => (
+  <MyRouter.routerContext.Consumer>
+    {({ changePath }) => {
+      const handleClick = () => {
+        changePath("/cart");
+      };
+      return <ProductItem product={product} onClick={handleClick} />;
+    }}
+  </MyRouter.routerContext.Consumer>
+);
 
 export default OrderableProductItem;
