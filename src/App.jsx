25b7f7dc0ff0abf724fa486684b7b866efa92a16
chaerimjo-web <chaerim010710@gmail.com>
2025-06-27 14:17:47 +0900

    다이얼러그 제어

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import createEventEmitter from "shared/lib/EventEmitter";
 import * as MyRouter from "./lib/MyRouter";
+import * as MyLayout from "./lib/MyLayout";
 import CartPage from "./pages/CartPage";
 import OrderPage from "./pages/OrderPage";
 import ProductPage from "./pages/ProductPage";
 import { getComponentName } from "./lib/utils";
+import Dialog from "./components/Dialog";
+import Backdrop from "./components/Backdrop";
+import { Layout } from "./lib/MyLayout";
 
 const App = () => (
-  <MyRouter.Router>
-    <MyRouter.Routes>
-      <MyRouter.Route path={"/cart"} element={<CartPage />} />
-      <MyRouter.Route path={"/order"} element={<OrderPage />} />
-      <MyRouter.Route path={"/"} element={<ProductPage />} />
-    </MyRouter.Routes>
-  </MyRouter.Router>
+  <MyLayout.Layout>
+    <MyRouter.Router>
+      <MyRouter.Routes>
+        <MyRouter.Route path={"/cart"} element={<CartPage />} />
+        <MyRouter.Route path={"/order"} element={<OrderPage />} />
+        <MyRouter.Route path={"/"} element={<ProductPage />} />
+      </MyRouter.Routes>
+    </MyRouter.Router>
+  </MyLayout.Layout>
 );
 
 export default App;
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import * as MyLayout from "../lib/MyLayout";
+import Backdrop from "../components/Backdrop";
+import Dialog from "./Dialog";
 
 const Page = ({ header, children, footer }) => (
   <div className="Page">
     <header>{header}</header>
     <main>{children}</main>
-    <footer>{footer}</footer>
+    {/* <footer>{footer}</footer> */}
+    <MyLayout.DialogContainer />
+    <MyLayout.layoutContext.Consumer>
+      {({ setDialog }) => (
+        <button
+          onClick={() => {
+            setDialog(<Dialog />);
+          }}
+        >
+          다이얼러그 버튼
+        </button>
+      )}
+    </MyLayout.layoutContext.Consumer>
   </div>
 );
 
